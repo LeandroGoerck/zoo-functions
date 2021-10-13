872e165c4407ce26b9eb4169f34249a0913e1a24
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-10-13 17:03:56 -0300

    concluido itens 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specieObj = species.find((specie) => specie.name === animal);
+  const residents = specieObj.residents.every((resident) => resident.age > age);
+  return residents;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  const newArray = [];
+  ids.forEach((specie) => newArray.push(species.find((specie2) => specie2.id === specie)));
+  return newArray;
 }
 
 module.exports = getSpeciesByIds;
