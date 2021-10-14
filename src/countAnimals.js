f6e8e660a27ee2f8c8520570cea8993b806bf760
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-10-14 17:02:14 -0300

    concluido exercicio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    const { specie: specie2 } = animal;
+    const spec = species.find((specie) => specie.name === specie2);
+    return spec.residents.length;
+  }
+  const { specie: specie2 } = animal;
+  const spec = species.find((specie) => specie.name === specie2);
+  return spec.residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
+
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'penguins' }));
+// console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
