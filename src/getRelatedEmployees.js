9b936dbc4dce32e95d7404049a42b0cbfcd52ee7
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-10-14 15:04:01 -0300

    concluido exercicio 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,27 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let isManagerFlag = false;
+  employees.forEach((employee) => {
+    if (employee.managers.some((mangID) => mangID === id) === true) {
+      isManagerFlag = true;
+    }
+  });
+  return isManagerFlag;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const arrayNames = [];
+  if (isManager(managerId)) {
+    employees.forEach((employee, index) => {
+      if (employee.managers.some((manIds) => manIds === managerId)) {
+        arrayNames.push(`${employee.firstName} ${employee.lastName}`);
+      }
+    });
+    return arrayNames;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
